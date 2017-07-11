ecc0b9b13ea32143d9e0dc71898e1bf060c838cd
Jack Zuban <jack.zuban@gmail.com>
2017-07-10 20:38:21 -0500

    add REview model and migration for creating reviews table

---
+++ database/migrations/2017_07_11_013628_create_reviews_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('review');
+            $table->boolean('sentiment');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
