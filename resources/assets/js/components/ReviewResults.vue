925c6927ff96ba9f7567075c474d54f76b463b44
Jack Zuban <jack.zuban@gmail.com>
2017-07-24 23:10:53 -0500

    build review results page structure

--- resources/assets/js/components/ReviewResults.vue
+++ resources/assets/js/components/ReviewResults.vue
@@ -1,5 +1,36 @@
 <template>
   <div>
-    Movie review results.
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h3 class="panel-title">Review prediction results:</h3>
+      </div>
+
+      <div class="panel-body">
+        <blockquote>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.
+        </blockquote>
+
+        <p>
+          <strong>This review is: </strong>positive
+        </p>
+
+        <p>
+          <strong>Probability: </strong>80.0%
+        </p>
+      </div>
+
+      <div class="panel-footer">
+        <button type="button" class="btn btn-success">Correct</button>
+        <button type="button" class="btn btn-warning">Incorrect</button>
+      </div>
+    </div>
+
+    <p>
+      <button type="button" class="btn btn-primary">Submit another review</button>
+    </p>
   </div>
 </template>
+
+<script>
+
+</script>
