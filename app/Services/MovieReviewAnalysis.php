eec4f769f0eadbe98750455ae8a8c62c6b039ceb
Jack Zuban <jack.zuban@gmail.com>
2017-07-28 22:59:03 -0500

    MovieReviewAnalysis service init

---
+++ app/Services/MovieReviewAnalysis.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace app\Services;
+
+/**
+ * Class MovieReviewAnalysis a service class for analyzing text reviews.
+ *
+ * @package app\Services
+ */
+class MovieReviewAnalysis
+{
+    public function analyzeReview()
+    {
+        //TODO: implement method
+    }
+}
