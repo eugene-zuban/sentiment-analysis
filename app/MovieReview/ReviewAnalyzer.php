3dde025fd97378436c3335417296f0100220f5c1
Jack Zuban <jack.zuban@gmail.com>
2017-07-31 20:56:53 -0500

    using ClassifiedReview data object for movie review classification

--- app/MovieReview/ReviewAnalyzer.php
+++ app/MovieReview/ReviewAnalyzer.php
@@ -10,15 +10,40 @@ namespace App\MovieReview;
 class ReviewAnalyzer
 {
     /**
+     * @var \App\MovieReview\ReviewDataObjectsFactory
+     */
+    protected $reviewFactory;
+
+    /**
+     * @param \App\MovieReview\ReviewDataObjectsFactory $reviewFactory
+     */
+    public function __construct(ReviewDataObjectsFactory $reviewFactory)
+    {
+        $this->reviewFactory = $reviewFactory;
+    }
+
+    /**
+     * Classify the provided text review using saved logistic regression model
+     *
      * @param string $textReview
-     * @return object
+     * @return ClassifiedReview
      */
     public function classify($textReview)
     {
-        return (object) [
-            'providedReview' => $textReview,
-            'predictedClass' => 'positive',
-            'predictedProbability' => '80.0%',
-        ];
+        return $this->getDummyClassifiedReview($textReview);
+    }
+
+    /**
+     * @param string $textReview
+     * @return \App\MovieReview\ClassifiedReview
+     */
+    protected function getDummyClassifiedReview($textReview)
+    {
+        $classifiedReview = $this->reviewFactory->makeEmptyClassifiedReview();
+        $classifiedReview->setProvidedReviewText($textReview);
+        $classifiedReview->setPredictedClass(true);
+        $classifiedReview->setPredictedProbability(80.99);
+
+        return $classifiedReview;
     }
 }
