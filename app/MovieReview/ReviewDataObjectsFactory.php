329fe3ff1b84d5c76e128b5002a1d7a3b1035964
Jack Zuban <jack.zuban@gmail.com>
2017-07-31 20:08:33 -0500

    add a factory for creating Review and ClassifiedReview objects

---
+++ app/MovieReview/ReviewDataObjectsFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\MovieReview;
+
+/**
+ * Class ReviewDataObjectsFactory
+ * Provides an interface for making Review & ClassifiedReview data objects
+ *
+ * @package app\MovieReview
+ */
+class ReviewDataObjectsFactory
+{
+    /**
+     * @return \app\MovieReview\Review
+     */
+    public function makeEmptyReview()
+    {
+        return new Review();
+    }
+
+    /**
+     * @return \app\MovieReview\ClassifiedReview
+     */
+    public function makeEmptyClassifiedReview()
+    {
+        return new ClassifiedReview();
+    }
+}
