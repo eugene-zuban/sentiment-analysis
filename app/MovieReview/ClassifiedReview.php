e7bc022624a278fa1192bd30f7dd3ccff03ce57c
Jack Zuban <jack.zuban@gmail.com>
2017-07-31 20:37:06 -0500

    make the ClassifiedReview class as a smart interface for working with Review object directly

--- app/MovieReview/ClassifiedReview.php
+++ app/MovieReview/ClassifiedReview.php
@@ -2,6 +2,13 @@
 
 namespace App\MovieReview;
 
+/**
+ * Class ClassifiedReview
+ * A container object using during a review classification (prediction) process
+ * and stores Review data along with predicted probability.
+ *
+ * @package App\MovieReview
+ */
 class ClassifiedReview
 {
     /**
@@ -15,23 +22,60 @@ class ClassifiedReview
     protected $predictedProbability;
 
     /**
-     * @param \app\MovieReview\Review $review
+     * ClassifiedReview constructor.
+     * ClassifiedReview must be initialized with a Review object.
+     *
+     * @param \App\MovieReview\Review $review
      */
-    public function setReview(Review $review)
+    public function __construct(Review $review)
     {
         $this->review = $review;
     }
 
     /**
-     * @return \app\MovieReview\Review
+     * Set analyzed review text.
+     *
+     * @param string $reviewText
      */
-    public function getReview()
+    public function setProvidedReviewText($reviewText)
     {
-        return $this->review;
+        $this->review->setText($reviewText);
     }
 
+    /**
+     * Return original/provided review text.
+     *
+     * @return string
+     */
+    public function getProvidedReviewText()
+    {
+        return $this->review->getText();
+    }
 
     /**
+     * Set positive or negative predicted label.
+     *
+     * @param bool $isPositive
+     */
+    public function setPredictedClass($isPositive = true)
+    {
+        $this->review->setSentiment($isPositive);
+    }
+
+    /**
+     * Returns predicted class label.
+     * We are using bool so predicted class can be positive or negative only.
+     *
+     * @return bool
+     */
+    public function isPredictedClassPositive()
+    {
+        return $this->review->isPositive();
+    }
+
+    /**
+     * Set probability that class label is correct.
+     *
      * @param float $probability
      */
     public function setPredictedProbability($probability)
@@ -40,10 +84,23 @@ class ClassifiedReview
     }
 
     /**
+     * Get probability that class label is correct.
+     *
      * @return float
      */
     public function getPredictedProbability()
     {
         return $this->predictedProbability;
+    }
+
+    /**
+     * Return the current Review object.
+     * It's a service method in case if we want to get Review object directly.
+     *
+     * @return \app\MovieReview\Review
+     */
+    public function getReview()
+    {
+        return $this->review;
     }
 }
