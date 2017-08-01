fa38328b3bf468fb10793e5034c84776faab5eef
Jack Zuban <jack.zuban@gmail.com>
2017-07-31 19:48:32 -0500

    sentiment and text setters & getters

--- app/MovieReview/Review.php
+++ app/MovieReview/Review.php
@@ -6,5 +6,35 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    /**
+     * @param string $reviewText
+     */
+    public function setText($reviewText)
+    {
+        $this->text = $reviewText;
+    }
+
+    /**
+     * @return string
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * @param bool $sentiment
+     */
+    public function setSentiment($sentiment)
+    {
+        $this->sentiment = $sentiment;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isPositive()
+    {
+        return $this->sentiment;
+    }
 }
