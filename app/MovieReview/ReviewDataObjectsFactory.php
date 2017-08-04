fad26d25ef7c6b277e23dde7240a62b4b5860f76
Jack Zuban <jack.zuban@gmail.com>
2017-08-04 01:08:14 -0500

    make Review from AddMovieReviewRequest

--- app/MovieReview/ReviewDataObjectsFactory.php
+++ app/MovieReview/ReviewDataObjectsFactory.php
@@ -2,6 +2,8 @@
 
 namespace App\MovieReview;
 
+use App\Http\Requests\Api\AddMovieReviewRequest;
+
 /**
  * Class ReviewDataObjectsFactory
  * Provides an interface for making Review & ClassifiedReview data objects
@@ -16,6 +18,21 @@ class ReviewDataObjectsFactory
     public function makeEmptyReview()
     {
         return new Review();
+    }
+
+    /**
+     * @param \App\Http\Requests\Api\AddMovieReviewRequest $request
+     * @return \app\MovieReview\Review
+     */
+    public function makeReviewUsingRequest(AddMovieReviewRequest $request)
+    {
+        $review = $this->makeEmptyReview();
+
+        $review->setText($request->input('reviewText'));
+
+        $review->setSentiment($request->input('sentiment'));
+
+        return $review;
     }
 
     /**
