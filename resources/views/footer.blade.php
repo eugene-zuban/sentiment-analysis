a8801dba79e0ae4daa02d40eeac7b1ceabd8597f
Jack Zuban <jack.zuban@gmail.com>
2017-08-05 15:38:01 -0500

    add footer

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,13 @@
+<footer>
+    <row class="text-center">
+        <p>
+            Logistic Regression Classifier is <a href="http://scikit-learn.org/stable/modules/generated/sklearn.linear_model.SGDClassifier.html">SGDClassifier from scikit-learn library</a> (Python).<br>
+            Machine Learning model was initially trained on <a href="http://ai.stanford.edu/~amaas/data/sentiment">movie reviews dataset</a> from IMDb.<br>
+        </p>
+
+        <p>
+            Project's codebase is available on GitHub <a href="https://github.com/jack-zuban/sentiment-analysis">sentiment-analysis repository</a>.
+        </p>
+        <p></p>
+    </row>
+</footer>
