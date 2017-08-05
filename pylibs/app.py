6c53d21620bd28bd7e4a081cae32694b9b006099
Jack Zuban <jack.zuban@gmail.com>
2017-08-04 20:50:21 -0500

    using tra/catch while performing classification

--- pylibs/app.py
+++ pylibs/app.py
@@ -3,22 +3,22 @@ import numpy as np
 from vectorizer import vect
 
 def main(review):
-    #try:
-    classifier = pickle.load(open(os.path.join('pylibs/pkl_objects', 'classifier.pkl'), 'rb'))
+    try:
+        classifier = pickle.load(open(os.path.join('pylibs/pkl_objects', 'classifier.pkl'), 'rb'))
 
-    transformedReview = vect.transform([review])
-    predicted_class = classifier.predict(transformedReview)[0]
-    predicted_probability = np.max(classifier.predict_proba(transformedReview)) * 100
+        transformedReview = vect.transform([review])
+        predicted_class = classifier.predict(transformedReview)[0]
+        predicted_probability = np.max(classifier.predict_proba(transformedReview)) * 100
 
-    print(json.dumps({
-        'is_positive': str(predicted_class),
-        'probability': str(predicted_probability),
-        'review': review
-    }))
+        print(json.dumps({
+            'is_positive': str(predicted_class),
+            'probability': str(predicted_probability),
+            'review': review
+        }))
 
-    #except Exception as e:
-    #    print(e)
-    #    sys.exit()
+    except Exception as e:
+        print(e)
+        sys.exit()
 
 if __name__ == "__main__":
    main(sys.stdin.read())
